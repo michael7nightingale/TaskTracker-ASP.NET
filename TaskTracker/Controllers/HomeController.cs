2c472c85856b439e47bb13ebe8e384e26106f126
michael7nightingale <suslanchikmopl@gmail.com>
2023-10-01 00:05:07 +0500

    correct duplicated url

--- TaskTracker/Controllers/HomeController.cs
+++ TaskTracker/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ public class HomeController : Controller
     public IActionResult Index() => View();
 
     [HttpGet]
-    [Route("/")]
+    [Route("/privacy")]
     public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
