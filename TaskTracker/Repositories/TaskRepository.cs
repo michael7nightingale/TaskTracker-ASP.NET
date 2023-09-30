b638f9294c28c590c6d22aefff8d49a0bb99da3d
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-30 13:47:44 +0500

    add get method

--- TaskTracker/Repositories/TaskRepository.cs
+++ TaskTracker/Repositories/TaskRepository.cs
@@ -16,5 +16,19 @@ public class TaskRepository : BaseRepository<Task.Task>
     {
         return await Entity.Where(t => t.DashBoard.Id == id).ToListAsync();
     }
-    
+
+
+    public async Task<Task.Task?> GetByDashBoardId(string id, string dashBoardId)
+    {
+        return await Entity.FirstOrDefaultAsync(p => p.Id == id && p.DashBoardId == dashBoardId);
+    }
+
+    public void FinishTask(ref Task.Task task)
+    {
+        if (!task.IsFinished)
+        {
+            task.IsFinished = true;
+            SaveChanges();
+        }
+    }
 }
--- TaskTracker/ViewModels/DashBoard/TaskCreateModel.cs
+++ TaskTracker/ViewModels/DashBoard/TaskCreateModel.cs
@@ -5,6 +5,6 @@ namespace TaskTracker.ViewModels.DashBoard;
 public class TaskCreateModel
 {
     public string Title { get; set; }
-    public string Desciprion { get; set; }
+    public string Description { get; set; }
     public List<User> ToUsers { get; set; } = new();
 }
